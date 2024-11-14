2f838e6c9d3689ab02584622b173f70bc6dd16b5
Maina <smurkcarter855@gmail.com>
2024-11-14 22:03:12 +0300

    Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
 import './App.css'
 import About from './components/About'
 import ShoeCategoryPage from './components/ShoeProduct'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from './sorce/HomePage';
+import Cart from './sorce/Cart';
+
+
 
 function App() {
 
@@ -8,6 +13,12 @@ function App() {
     <>
     <ShoeCategoryPage/>
     <About/>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
+    </Router>
     </>
   )
 }
