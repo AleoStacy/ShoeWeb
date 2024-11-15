d6b43ab04d668d0a02cf3822f7ee1ee185e92315
Maxi-arch <mxmu025@gmail.com>
2024-11-15 10:33:08 +0300

    4thcommit

--- shoe/src/App.jsx
+++ shoe/src/App.jsx
@@ -3,6 +3,9 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './HomePage';
 import Cart from './Cart'; 
+import shoeCategoryPage from "./src/components/ProductList.jsx"
+import About from "./ShoeWeb/src/components/ContactPage.jsx"
+
 
 function App() {
   return (
@@ -11,7 +14,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/cart" element={<Cart />} />
         <Route path ="/contact" element={<Contact />}/>
-        <Route path="/shoeCategoryPage" element={<Store/>} />
+        <Route path="/productlist" element={<Store/>} />
         <Route path="/about" element={<About/>}/>
       </Routes>
     </Router>
